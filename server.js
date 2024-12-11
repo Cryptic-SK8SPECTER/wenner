2d3c091aa50f475a37a7867f7eadc35dfd03f1b4
Cryptic-SK8SPECTER <albertoveloz234@gmail.com>
2024-12-11 14:12:00 +0200

    adding new changes

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// const DB = process.env.DATABASE.replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
-  // .connect(DB, {
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+  // .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })

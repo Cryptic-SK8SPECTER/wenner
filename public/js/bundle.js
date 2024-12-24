d359f53ee480998808330f35431d146793c86342
Cryptic-SK8SPECTER <albertoveloz234@gmail.com>
2024-12-24 15:04:20 +0200

    wenners new updates v1.1

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1756,7 +1756,7 @@ var logout = exports.logout = /*#__PURE__*/function () {
           });
         case 3:
           res = _context2.sent;
-          if (res.data.status = 'success') location.reload(true);
+          if (res.data.status === 'success') location.reload(true);
           _context2.next = 11;
           break;
         case 7:
@@ -2780,7 +2780,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51168" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1858" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
